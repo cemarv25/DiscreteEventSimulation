0bcf39e67f3b52815130fa041527f692e5fb96de
Cesar Martinez <cesar@Cesars-MacBook-Air.local>
2022-03-25 12:48:32 -0700

    added the option for a and m to not be present in cong_mult

--- scripts/congruencial_mult.py
+++ scripts/congruencial_mult.py
@@ -1,4 +1,10 @@
-def congruencial_mult(seed, k, g, a, m, n):
+def congruencial_mult(seed, k, g, n, a, m):
+  if not a:
+    a = 5 + 8 * int(k)
+
+  if not m:
+    m = pow(2, int(g))
+
   i = 0
   xn_minus_one = seed
   xn = 0
@@ -19,8 +25,17 @@ if __name__ == "__main__":
   seed = input('escribe la semilla: ')
   k = input('escribe el valor para k: ')
   g = input('escribe el valor para g: ')
-  a = input('escribe el valor para a: ')
-  m = input('escribe el valor para m: ')
+
+  has_a = int(input('se tiene un valor para a? (1= sí, 0= no)'))
+  a = None
+  if has_a:
+    a = input('escribe el valor para a: ')
+    
+  has_m = int(input('se tiene un valor para m? (1= sí, 0= no)'))
+  m = None
+  if has_m:
+    m = input('escribe el valor para m: ')
+    
   n = int(input('ahora escribe cuantos numeros quieres generar: '))
 
-  print(congruencial_mult(seed, k, g, a, m, n))
+  print(congruencial_mult(seed, k, g, n, a, m))
